f208c90215bf93059b51a235f0c42daedff471d0
Anas Ahmed <anas31197@gmail.com>
2023-02-01 12:31:13 +0530

    Update authUser.controllers.js

--- src/controllers/authUser.controllers.js
+++ src/controllers/authUser.controllers.js
@@ -25,10 +25,6 @@ const loginUser = (req, res) => {
                     expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN
                 });
 
-                res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-                res.header('Access-Control-Allow-Credentials', true);
-
                 // jwt set in cookie
                 res.cookie('access_token', access_token, {
                     httpOnly: true,
